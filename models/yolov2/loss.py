9b19b0c8897976d8cefc6761f3cc8ded04b6fc5b
yjh0410 <1394571815@qq.com>
2023-03-24 09:03:12 +0800

    debug loss of YOLOv2

--- models/yolov2/loss.py
+++ models/yolov2/loss.py
@@ -83,6 +83,7 @@ class Criterion(object):
         
         # obj loss
         # gt_objectness[pos_masks] *= ious.clamp(0.)
+        print(ious)
         loss_obj = self.loss_objectness(pred_obj, gt_objectness)
         loss_obj = loss_obj.sum() / num_fgs
 
