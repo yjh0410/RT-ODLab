20de59b2e797d5b9d27dabeb36530e78c3884438
yjh0410 <1394571815@qq.com>
2023-03-24 16:58:15 +0800

    train YOLOv4 on VOC with 150 epoch

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py \
         --cuda \
         -d voc \
         --root /mnt/share/ssd2/dataset/ \
-        -m yolov3 \
+        -m yolov4 \
         -bs 16 \
         -size 640 \
         --wp_epoch 1 \
