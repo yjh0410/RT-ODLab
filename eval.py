4cc109ec9fe224549e0af50888e837764ddd8bb7
yjh0410 <1394571815@qq.com>
2023-04-22 13:33:05 +0800

    debug YOLOv7

--- eval.py
+++ eval.py
@@ -31,9 +31,9 @@ def parse_args():
                         help='build yolo')
     parser.add_argument('--weight', default=None,
                         type=str, help='Trained state_dict file path to open')
-    parser.add_argument('--conf_thresh', default=0.001, type=float,
-                        help='NMS threshold')
-    parser.add_argument('--nms_thresh', default=0.6, type=float,
+    parser.add_argument('-ct', '--conf_thresh', default=0.001, type=float,
+                        help='confidence threshold')
+    parser.add_argument('-nt', '--nms_thresh', default=0.6, type=float,
                         help='NMS threshold')
     parser.add_argument('--topk', default=1000, type=int,
                         help='topk candidates for testing')
