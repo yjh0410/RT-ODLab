446910ee7433dcbca72ed9f68472173ea1c3d6da
yjh0410 <1394571815@qq.com>
2023-04-26 11:31:19 +0800

    keep training YOLOv7-Tiny

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -m yolov7_large \
+        -m yolov7_tiny \
         -bs 16 \
         -size 640 \
         --wp_epoch 1 \
@@ -12,7 +12,7 @@ python train.py \
         --ema \
         --fp16 \
         --multi_scale \
-        --resume weights/coco/yolov7_large/yolov7_large_epoch_121_43.63.pth \
+        --resume weights/coco/yolov7_tiny/yolov7_tiny_epoch_231_35.19.pth \
         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
         # --eval_first
 
