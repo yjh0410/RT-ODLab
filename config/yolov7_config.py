db11724eac9dc373b93ef080ad70da9b00e0940f
yjh0410 <1394571815@qq.com>
2023-04-27 22:31:02 +0800

    Anchor-free YOLOv7

--- config/yolov7_config.py
+++ config/yolov7_config.py
@@ -1,68 +1,10 @@
 # YOLOv7 Config
 
 yolov7_cfg = {
-    'yolov7_nano':{
-        # input
-        'trans_type': 'yolov5_nano',
-        'multi_scale': [0.5, 1.0], # 320 -> 640
-        # model
-        'backbone': 'elannet_nano',
-        'pretrained': True,
-        'bk_act': 'lrelu',
-        'bk_norm': 'BN',
-        'bk_dpw': True,
-        'stride': [8, 16, 32],  # P3, P4, P5
-        # neck
-        'neck': 'sppf',
-        'expand_ratio': 0.5,
-        'pooling_size': 5,
-        'neck_act': 'lrelu',
-        'neck_norm': 'BN',
-        'neck_depthwise': True,
-        # fpn
-        'fpn': 'yolov7_pafpn',
-        'fpn_act': 'lrelu',
-        'fpn_norm': 'BN',
-        'fpn_depthwise': True,
-        'nbranch': 2.0,        # number of branch in ELANBlockFPN
-        'depth': 1.0,          # depth factor of each branch in ELANBlockFPN
-        'width': 0.25,         # width factor of channel in FPN
-        # head
-        'head': 'decoupled_head',
-        'head_act': 'lrelu',
-        'head_norm': 'BN',
-        'num_cls_head': 2,
-        'num_reg_head': 2,
-        'head_depthwise': True,
-        # matcher
-        'matcher': {'center_sampling_radius': 2.5,
-                    'topk_candicate': 10},
-        # loss weight
-        'loss_obj_weight': 1.0,
-        'loss_cls_weight': 1.0,
-        'loss_box_weight': 5.0,
-        # training configuration
-        'no_aug_epoch': 20,
-        # optimizer
-        'optimizer': 'sgd',        # optional: sgd, adam, adamw
-        'momentum': 0.937,         # SGD: 0.937;    AdamW: invalid
-        'weight_decay': 5e-4,      # SGD: 5e-4;     AdamW: 5e-2
-        'clip_grad': 10,           # SGD: 10.0;     AdamW: -1
-        # model EMA
-        'ema_decay': 0.9999,       # SGD: 0.9999;   AdamW: 0.9998
-        'ema_tau': 2000,
-        # lr schedule
-        'scheduler': 'linear',
-        'lr0': 0.01,               # SGD: 0.01;     AdamW: 0.001
-        'lrf': 0.01,               # SGD: 0.01;     AdamW: 0.01
-        'warmup_momentum': 0.8,
-        'warmup_bias_lr': 0.1,
-    },
-
     'yolov7_tiny':{
         # input
         'trans_type': 'yolov5_weak',
-        'multi_scale': [0.5, 1.0], # 320 -> 640
+        'multi_scale': [0.5, 1.5], # 320 -> 640
         # model
         'backbone': 'elannet_tiny',
         'pretrained': True,
@@ -120,7 +62,7 @@ yolov7_cfg = {
     'yolov7_large':{
         # input
         'trans_type': 'yolov5_strong',
-        'multi_scale': [0.5, 1.0], # 320 -> 640
+        'multi_scale': [0.5, 1.25], # 320 -> 640
         # model
         'backbone': 'elannet_large',
         'pretrained': True,
@@ -178,7 +120,7 @@ yolov7_cfg = {
     'yolov7_huge':{
         # input
         'trans_type': 'yolov5_strong',
-        'multi_scale': [0.5, 1.0], # 320 -> 640
+        'multi_scale': [0.5, 1.25], # 320 -> 640
         # model
         'backbone': 'elannet_huge',
         'pretrained': True,
