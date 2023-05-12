450416e5b2c23f9dd0b65a0915744a0c2d8f49a8
yjh0410 <1394571815@qq.com>
2023-05-12 16:06:46 +0800

    keep training YOLOv3-Tiny from epoch 141

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -m yolov5_s \
+        -m yolov3_t \
         -bs 16 \
         -size 640 \
         --wp_epoch 1 \
@@ -12,7 +12,7 @@ python train.py \
         --ema \
         --fp16 \
         --multi_scale \
-        # --resume weights/coco/yolov5_l/yolov5_l_epoch_211_46.56.pth \
+        --resume weights/coco/yolov3_t/yolov3_t_epoch_141_22.32.pth \
         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
         # --eval_first
 
