e8d1a88a7f3d40da9174a5bfdedd4d8232d51a5e
yjh0410 <1394571815@qq.com>
2023-05-12 09:27:46 +0800

    update train_ddp.sh

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,14 +1,15 @@
-# 8 GPUs
+# train YOLO with 8 GPUs
+# 使用8张GPU来训练YOLO
 python -m torch.distributed.run --nproc_per_node=8 train.py \
                                                     --cuda \
                                                     -dist \
                                                     -d coco \
                                                     --root /data/datasets/ \
-                                                    -m yolov1 \
+                                                    -m yolov5_l \
                                                     -bs 128 \
                                                     -size 640 \
                                                     --wp_epoch 1 \
-                                                    --max_epoch 150 \
+                                                    --max_epoch 300 \
                                                     --eval_epoch 10 \
                                                     --ema \
                                                     --fp16 \
