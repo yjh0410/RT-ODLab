753c60135c7ec934af014677f888eac547fd38ef
yjh0410 <1394571815@qq.com>
2023-05-25 08:35:11 +0800

    debug YOLOv5-Plus-S

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -m yolov5_plus_n \
+        -m yolov5_plus_s \
         -bs 16 \
         -size 640 \
         --wp_epoch 1 \
