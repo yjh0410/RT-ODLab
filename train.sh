fda13c908ce48e24d94029ba0852d51cc7e99478
yjh0410 <1394571815@qq.com>
2023-06-29 07:59:28 +0800

    debug rtdetr_n on coco

--- train.sh
+++ train.sh
@@ -3,15 +3,17 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -m yolox_m \
+        -m rtdetr_n \
         -bs 16 \
         -size 640 \
         --wp_epoch 3 \
-        --max_epoch 300 \
+        --max_epoch 150 \
         --eval_epoch 10 \
         --ema \
         --fp16 \
         --multi_scale \
+        --mosaic 0 \
+        --mixup 0
         # --resume weights/coco/yolox_s/yolox_s_best.pth \
         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
         # --eval_first
