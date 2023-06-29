5fc9eb2661e609bf82313600b5b34fd291beeb97
yjh0410 <1394571815@qq.com>
2023-06-29 19:46:21 +0800

    keep training YOLOX-M

--- train.sh
+++ train.sh
@@ -1,20 +1,39 @@
 # Train YOLO
 python train.py \
         --cuda \
-        -d voc \
+        -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -m rtdetr_n \
+        -m yolox_m \
         -bs 16 \
         -size 640 \
-        --wp_epoch 1 \
-        --max_epoch 150 \
+        --wp_epoch 3 \
+        --max_epoch 300 \
         --eval_epoch 10 \
         --ema \
         --fp16 \
         --multi_scale \
-        --mosaic 0 \
-        --mixup 0
-        # --resume weights/coco/yolox_s/yolox_s_best.pth \
+        --resume weights/coco/yolox_m/yolox_m_best.pth \
         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
         # --eval_first
+
+
+# # Train RT-DETR
+# python train.py \
+#         --cuda \
+#         -d voc \
+#         --root /mnt/share/ssd2/dataset/ \
+#         -m rtdetr_n \
+#         -bs 16 \
+#         -size 640 \
+#         --wp_epoch 1 \
+#         --max_epoch 150 \
+#         --eval_epoch 10 \
+#         --ema \
+#         --fp16 \
+#         --multi_scale \
+#         --mosaic 0 \
+#         --mixup 0
+#         # --resume weights/coco/yolox_s/yolox_s_best.pth \
+#         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
+#         # --eval_first
 
