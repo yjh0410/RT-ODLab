ea2b6407db52cceb9cb286d56fea5b8a3584189d
yjh0410 <1394571815@qq.com>
2023-07-01 14:28:00 +0800

    use RTMTrainer for YOLOvx

--- config/model_config/yolovx_config.py
+++ config/model_config/yolovx_config.py
@@ -121,16 +121,16 @@ yolovx_cfg = {
         'no_aug_epoch': 20,
         'trainer_type': 'yolo',
         # optimizer
-        'optimizer': 'sgd',        # optional: sgd, adam, adamw
-        'momentum': 0.9,           # SGD: 0.937;    AdamW: invalid
-        'weight_decay': 5e-4,      # SGD: 5e-4;     AdamW: 5e-2
-        'clip_grad': 10,           # SGD: 10.0;     AdamW: -1
+        'optimizer': 'adamw',      # optional: sgd, adam, adamw
+        'momentum': None,          # SGD: 0.937;    AdamW: invalid
+        'weight_decay': 5e-2,      # SGD: 5e-4;     AdamW: 5e-2
+        'clip_grad': 35,           # SGD: 10.0;     AdamW: -1
         # model EMA
-        'ema_decay': 0.9999,       # SGD: 0.9999;   AdamW: 0.9998
+        'ema_decay': 0.9998,       # SGD: 0.9999;   AdamW: 0.9998
         'ema_tau': 2000,
         # lr schedule
-        'scheduler': 'linear',
-        'lr0': 0.01,               # SGD: 0.01;     AdamW: 0.001
+        'scheduler': 'cos_linear',
+        'lr0': 0.001,              # SGD: 0.01;     AdamW: 0.001
         'lrf': 0.01,               # SGD: 0.01;     AdamW: 0.01
         'warmup_momentum': 0.8,
         'warmup_bias_lr': 0.1,
