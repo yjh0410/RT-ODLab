f9f36a6528cf165ee94f9ba2c0d1d9bd4329bf0a
yjh0410 <1394571815@qq.com>
2023-07-20 07:45:51 +0800

    train YOLOvx-T on COCO

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,11 +1,11 @@
-# train YOLO with 4 GPUs
+# train YOLO with 8 GPUs
 # 使用 8 GPU来训练YOLO
 python -m torch.distributed.run --nproc_per_node=8 train.py \
                                                     --cuda \
                                                     -dist \
                                                     -d coco \
                                                     --root /data/datasets/ \
-                                                    -m yolovx_s \
+                                                    -m yolovx_t \
                                                     -bs 128 \
                                                     -size 640 \
                                                     --wp_epoch 3 \
