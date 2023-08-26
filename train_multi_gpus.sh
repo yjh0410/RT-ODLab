70d5452fe16ac045ab1d1a60f903a33538bad8f7
yjh0410 <1394571815@qq.com>
2023-08-26 18:44:32 +0800

    modify YOLOX-Nano Transform

--- train_multi_gpus.sh
+++ train_multi_gpus.sh
@@ -4,7 +4,7 @@ python -m torch.distributed.run --nproc_per_node=8 train.py \
                                                     -dist \
                                                     -d coco \
                                                     --root /data/datasets/ \
-                                                    -m rtcdet_t \
+                                                    -m rtcdet_n \
                                                     -bs 128 \
                                                     -size 640 \
                                                     --wp_epoch 3 \
