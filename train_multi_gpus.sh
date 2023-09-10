977e2aeeccaaac93c6c96d41d6c004d9d26c8f7d
yjh0410 <1394571815@qq.com>
2023-09-10 10:29:00 +0800

    debug RTCDet-S w/o INP

--- train_multi_gpus.sh
+++ train_multi_gpus.sh
@@ -15,6 +15,7 @@ python -m torch.distributed.run --nproc_per_node=8 train.py \
                                                     --fp16 \
                                                     --sybn \
                                                     --multi_scale \
+                                                    --weight weights/debug \
                                                     # --load_cache \
                                                     # --resume weights/coco/yolox_l/yolox_l_best.pth \
 
