8c0ee0b96a6a022e41c27ce91e22e29674c26cf3
yjh0410 <1394571815@qq.com>
2023-09-14 09:28:57 +0800

    update

--- config/model_config/rtcdet_config.py
+++ config/model_config/rtcdet_config.py
@@ -321,7 +321,7 @@ rtcdet_cfg = {
         'head_depthwise': False,
         # ---------------- Train config ----------------
         ## Input
-        'multi_scale': [0.5, 1.5], # 320 -> 960
+        'multi_scale': [0.5, 1.25], # 320 -> 800
         'trans_type': 'yolox_large',
         # ---------------- Assignment config ----------------
         ## Matcher
