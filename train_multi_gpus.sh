7081241bb9e543a345e75e85fd782e16ee9e72eb
yjh0410 <1394571815@qq.com>
2023-11-12 23:19:16 +0800

    train YOLOv8-N

--- train_multi_gpus.sh
+++ train_multi_gpus.sh
@@ -1,24 +1,3 @@
-# -------------------------- Train RTCDet --------------------------
-# python -m torch.distributed.run --nproc_per_node=8 train.py \
-#                                                     --cuda \
-#                                                     -dist \
-#                                                     -d coco \
-#                                                     --root /data/datasets/ \
-#                                                     -m rtcdet_s \
-#                                                     -bs 128 \
-#                                                     -size 640 \
-#                                                     --wp_epoch 3 \
-#                                                     --max_epoch 300 \
-#                                                     --eval_epoch 10 \
-#                                                     --no_aug_epoch 20 \
-#                                                     --ema \
-#                                                     --fp16 \
-#                                                     --sybn \
-#                                                     --multi_scale \
-#                                                     --save_folder weights/ \
-#                                                     # --load_cache \
-#                                                     # --resume weights/coco/yolox_l/yolox_l_best.pth \
-
 # -------------------------- Train YOLOX & YOLOv7 --------------------------
 # python -m torch.distributed.run --nproc_per_node=8 train.py \
 #                                                     --cuda \
