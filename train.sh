14dbaddfe70b606538a3eb5d22076703e882c409
yjh0410 <1394571815@qq.com>
2024-01-08 11:12:24 +0800

    update

--- train.sh
+++ train.sh
@@ -85,7 +85,7 @@ if [ $WORLD_SIZE == 1 ]; then
             --fp16 \
             --multi_scale
 elif [[ $WORLD_SIZE -gt 1 && $WORLD_SIZE -le 8 ]]; then
-    python -m torch.distributed.run --nproc_per_node=${WORLD_SIZE} train.py \
+    python -m torch.distributed.run --nproc_per_node=${WORLD_SIZE} --master_port 1669 train.py \
             --cuda \
             -dist \
             --dataset ${DATASET} \
